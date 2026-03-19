bc6b8d2fb6a6b11a205a2a886c8e294323a57542
ZAHID ASHIQ BADANA <113751984+Zaidikhanb@users.noreply.github.com>
2026-03-19 17:07:46 +0500

    Create db.js

---
+++ api/utils/db.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+
+let cachedClient = null;
+let cachedDb = null;
+
+async function connectToDatabase() {
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    throw new Error('Please define MONGODB_URI environment variable');
+  }
+
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  await client.connect();
+  const db = client.db('attendanceDB');
+
+  cachedClient = client;
+  cachedDb = db;
+
+  return { client, db };
+}
+
+module.exports = connectToDatabase;
